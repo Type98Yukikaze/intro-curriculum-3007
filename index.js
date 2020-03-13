bc7deb7a887812f8b5ac7a1f78a73afef9e39a1a
SERRE <yuki@TYPE-SERRE.localdomain>
2020-03-13 23:07:45 +0900

    17で割り切れるか判定する関数をモジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+ * 17で割り切れるときtrueを返す
+ * @param 
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+    if (num % 17 === 0) {
+        return true;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
